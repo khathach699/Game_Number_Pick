936da1881559975f220efebbc3f95b19821d2a4a
KhaThach <khathach699@gmail.com>
2025-02-17 13:16:05 +0700

    design lost,  try again

--- lib/src/pages/game/game_page.dart
+++ lib/src/pages/game/game_page.dart
@@ -55,19 +55,120 @@ class GamePage extends StatelessWidget {
                   ),
                   itemCount: 50,
                   itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color:
-                            Colors.primaries[index % Colors.primaries.length],
-                      ),
-                      child: Text(
-                        index.toString(),
-                        style: TextStyle(
-                          fontSize: 24.sp,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
+                    return Material(
+                      shape: CircleBorder(),
+                      clipBehavior: Clip.hardEdge,
+                      child: InkWell(
+                        splashColor: Colors.white,
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return Center(
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 15.dg,
+                                  ),
+                                  child: Container(
+                                    padding: EdgeInsets.symmetric(
+                                      vertical: 22.dg,
+                                      horizontal: 58.dg,
+                                    ),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(40.r),
+                                      color: Colors.white,
+                                    ),
+                                    width: 365.dg,
+                                    height: 265.dg,
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          "Sorry, your failed",
+                                          style: TextStyle(
+                                            fontSize: 24.sp,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        20.verticalSpace,
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              "Your core:",
+                                              style: TextStyle(
+                                                fontSize: 24.sp,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            10.horizontalSpace,
+                                            Text(
+                                              "20",
+                                              style: TextStyle(
+                                                fontSize: 24.sp,
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.red,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        55.verticalSpace,
+                                        Material(
+                                          borderRadius: BorderRadius.circular(
+                                            20.r,
+                                          ),
+                                          clipBehavior: Clip.hardEdge,
+                                          child: InkWell(
+                                            splashColor: Colors.yellowAccent,
+                                            onTap: () {},
+                                            child: Ink(
+                                              width: 214.dg,
+                                              height: 69.dg,
+                                              decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(20.r),
+                                                color: const Color(0xff1fc5ea),
+                                              ),
+
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                children: [
+                                                  Image.asset(
+                                                    "assets/icons/2.png",
+                                                  ),
+                                                  15.horizontalSpace,
+                                                  Text("Try Again"),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                        child: Ink(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color:
+                                Colors.primaries[index %
+                                    Colors.primaries.length],
+                          ),
+                          child: Center(
+                            child: Text(
+                              index.toString(),
+                              style: TextStyle(
+                                fontSize: 24.sp,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
                         ),
                       ),
                     );
